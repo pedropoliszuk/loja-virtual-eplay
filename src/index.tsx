2b648fbcb062d0cf16238d7f96d24b47d7ad462b
Pedro Poliszuk <pedropoliszuk2@live.com>
2024-06-30 22:10:51 -0300

    construindo o header

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
