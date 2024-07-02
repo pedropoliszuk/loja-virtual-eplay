c2eca1d5f96204877f42e6b7bb69bc044bd4b9b3
Pedro Poliszuk <pedropoliszuk2@live.com>
2024-07-02 12:35:32 -0300

    criando card de produto e lista de produtos

---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,51 @@
+import Product from '../Product'
+import { Container, List } from './styles'
+
+export type Props = {
+  title: string
+  background: 'gray' | 'black'
+}
+
+const ProductsList = ({ background, title }: Props) => (
+  <Container background={background}>
+    <div className="container">
+      <h2>{title}</h2>
+      <List>
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placehold.it/222x250"
+          infos={['-10%', 'R$ 150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+      </List>
+    </div>
+  </Container>
+)
+
+export default ProductsList
