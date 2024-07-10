be727e774cdf2fc87e449738ea9094549de98569
Pedro Poliszuk <pedropoliszuk2@live.com>
2024-07-10 12:30:17 -0300

    criando galeria e modal

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -2,6 +2,10 @@ import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
 import Section from '../../components/Section'
 
+import Gallery from '../../components/Gallery'
+
+import residentEvil from '../../assets/images/resident.png'
+
 const Product = () => {
   const { id } = useParams()
   return (
@@ -35,9 +39,7 @@ const Product = () => {
           jogo.
         </p>
       </Section>
-      <Section title="Galeira" background="black">
-        <div>fotos</div>
-      </Section>
+      <Gallery name="Jogo teste" defaultCover={residentEvil} />
     </>
   )
 }
