285ababffd048761ada99751bcef966025cc2af5
Pedro Poliszuk <pedropoliszuk2@live.com>
2024-07-14 23:37:26 -0300

    configurando o redux, criando api com rtk query e criando o aside do carrinho

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,19 +1,13 @@
 import { Imagem, Titulo, Precos } from './styles'
 import Tag from '../Tag'
 import Button from '../Button'
-import { Game } from '../../pages/Home'
-import { useEffect, useState } from 'react'
 
 import { formataPreco } from '../ProductsList'
 
-const Banner = () => {
-  const [game, setGame] = useState<Game>()
+import { useGetFeaturedGameQuery } from '../../services/api'
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque')
-      .then((res) => res.json())
-      .then((res) => setGame(res))
-  }, [])
+const Banner = () => {
+  const { data: game, isLoading } = useGetFeaturedGameQuery()
 
   if (!game) {
     return <h3>Carregando...</h3>
